1e00c1e05b44c2db0d558cdea337aab891405d3b
Dan Tovbein <dtovbein@gmail.com>
2021-01-28 09:10:52 -0300

    Implement fontFace function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import { PixelToRemFn } from "./types";
 
 export const pixelToRem: PixelToRemFn = (px: number, pxBase = 16) => `${(px / pxBase)}rem`;
+
+export const fontFace = (
+  fontFamily: string,
+  src: string,
+  fontWeight: number = 500,
+  fontStyle: string = 'normal',
+) => {
+  return `
+    @font-face {
+      font-family: ${fontFamily};
+      font-style: ${fontStyle};
+      font-weight: ${fontWeight};
+      src: url(${src});
+    }
+  `
+};
