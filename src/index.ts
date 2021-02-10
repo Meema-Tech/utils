73d04348824d1ffa4422cfd5e2ccfebec2e16fdb
Dan Tovbein <dtovbein@gmail.com>
2021-02-10 06:12:33 -0300

    Export font face function

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
-// import { PixelToRemFn } from "./types";
-export interface PixelToRemFn<> { 
+interface PixelToRemFn<> { 
   (px: number, pxBase?: number): string;
 }
-
 export const pixelToRem: PixelToRemFn = (px: number, pxBase = 16) => `${(px / pxBase)}rem`;
 
-export const fontFace = (
-  fontFamily: string,
-  src: string,
-  fontWeight: number = 500,
-  fontStyle: string = 'normal',
+
+interface FontFaceFn<> { 
+  (fontFamily: string, src: string, fontWeight?: number, fontStyle?: string): string;
+}
+export const fontFace: FontFaceFn = (
+  fontFamily,
+  src,
+  fontWeight = 500,
+  fontStyle = 'normal',
 ) => {
   return `
     @font-face {
