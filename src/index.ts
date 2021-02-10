c7c3f9b94bc4006e402612e11331ee98fb578996
Dan Tovbein <dtovbein@gmail.com>
2021-02-10 06:31:39 -0300

    Add mobile detection and CSS props

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+import { css, CSSProp, CSSObject } from "styled-components";
+export type CustomCSSType = CSSProp | CSSObject;
+
+export interface IElement {
+  customCss?: CustomCSSType;
+}
+
 interface PixelToRemFn<> { 
   (px: number, pxBase?: number): string;
 }
 export const pixelToRem: PixelToRemFn = (px: number, pxBase = 16) => `${(px / pxBase)}rem`;
 
-
 interface FontFaceFn<> { 
-  (fontFamily: string, src: string, fontWeight?: number, fontStyle?: string): string;
+  (fontFamily: string, src: string, fontWeight?: number, fontStyle?: string): CustomCSSType;
 }
 export const fontFace: FontFaceFn = (
   fontFamily,
@@ -13,7 +19,7 @@ export const fontFace: FontFaceFn = (
   fontWeight = 500,
   fontStyle = 'normal',
 ) => {
-  return `
+  return css`
     @font-face {
       font-family: ${fontFamily};
       font-style: ${fontStyle};
@@ -22,3 +28,10 @@ export const fontFace: FontFaceFn = (
     }
   `
 };
+
+interface IsMobileFn<> {
+  (): boolean;
+}
+export const isMobile: IsMobileFn = () => (
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(window.navigator.userAgent)
+);
